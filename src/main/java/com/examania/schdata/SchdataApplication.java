e440a1bef69077207acd081faa3b9700af4a93c3
Sachin Kumar <sachin8447@gmail.com>
2021-05-02 16:30:47 +0530

    sdf

---
+++ src/main/java/com/examania/schdata/SchdataApplication.java
@@ -0,0 +1,13 @@
+package com.examania.schdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchdataApplication.class, args);
+    }
+
+}
